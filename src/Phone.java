5d9abce26a977969968cfda5093722ef8e18befc
Вершинин Юрий <yvershinin@inno.tech>
2022-02-20 12:41:23 +0300

    Добавлен конструктор без параметров

--- src/Phone.java
+++ src/Phone.java
@@ -2,4 +2,14 @@ public class Phone {
     String model;
     int num;
     String soft;
+
+
+
+    public Phone(String model, int num, String soft) {
+        this.model = model;
+        this.num = num;
+        this.soft = soft;
+    }
+    public Phone(){};
+
 }
